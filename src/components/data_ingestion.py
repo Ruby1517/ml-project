e33c09c17d073e9840ba6e20460a356ef5cd185d
Ruby <ruby.hoseinzadeh@gmail.com>
2024-05-12 14:38:16 -0700

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+'''
+Data ingestion is the process of importing data from multiple sources into a cloud-based 
+storage medium, such as a data warehouse, database, or data mart. The data can come from 
+many different sources, including social media platforms, email marketing platforms, 
+financial systems, and CRM systems. The data is then sanitized and transformed into 
+a uniform format using an extract/transform/load (ETL) process. 
+
+'''
+
